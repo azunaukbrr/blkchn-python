41462f83ec1a73785dcd1d320147e4d4b091ecdb
azunaukbrr <ahsan.akbar@gmail.com>
2021-07-29 23:56:35 -0500

    files

--- wallet.py
+++ wallet.py
@@ -1,11 +1,37 @@
+# Import dependencies
 import subprocess
 import json
-from pprint import pprint
+from dotenv import load_dotenv
 
-command = "php hd-wallet-derive/hd-wallet-derive.php -g --mnemonic=\"toss sing pride history initial afford oval liar only lamp unveil educate\" --cols=path,address,privkey --format=json"
+# Load and set environment variables
+load_dotenv()
+mnemonic=os.getenv("mnemonic")
 
-p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-output, err = p.communicate()
-p.wait()
-keys = json.loads(output)
-pprint(keys)
+# Import constants.py and necessary functions from bit and web3
+# YOUR CODE HERE
+from constants import *
+ 
+ 
+# Create a function called `derive_wallets`
+def derive_wallets(# YOUR CODE HERE):
+    command = # YOUR CODE HERE
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    output, err = p.communicate()
+    p_status = p.wait()
+    return json.loads(output)
+
+# Create a dictionary object called coins to store the output from `derive_wallets`.
+coins = # YOUR CODE HERE
+
+# Create a function called `priv_key_to_account` that converts privkey strings to account objects.
+def priv_key_to_account(# YOUR CODE HERE):
+    # YOUR CODE HERE
+
+# Create a function called `create_tx` that creates an unsigned transaction appropriate metadata.
+def create_tx(# YOUR CODE HERE):
+    # YOUR CODE HERE
+
+# Create a function called `send_tx` that calls `create_tx`, signs and sends the transaction.
+def send_tx(# YOUR CODE HERE):
+    # YOUR CODE HERE
+
